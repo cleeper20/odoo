c24e6977533b03076596df127c229b0516eb0499
cleeper24 <52640762+cleeper24@users.noreply.github.com>
2020-07-10 19:32:48 -0600

    lfa 20200710

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -25,8 +25,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'templates.xml',
+        'security/ir.model.access.csv',
+        'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
