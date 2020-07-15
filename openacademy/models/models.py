936dd83b0281f1717f1841a54f7f9f5f0a37653f
cleeper24 <52640762+cleeper24@users.noreply.github.com>
2020-07-15 17:08:29 -0600

    lfa elaciones 15072020

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -3,6 +3,8 @@ name = fields.Char(string="Title", required=True)
 
          responsible_id = fields.Many2one('res.users',
         ondelete='set null', string="Responsible", index=True)
+        session_ids = fields.One2many(
+        'openacademy.session', 'course_id', string="Sessions")
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -16,3 +18,4 @@ class Session(models.Model):
     instructor_id = fields.Many2one('res.partner', string="Instructor")
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
