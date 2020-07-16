c77412185a25738206f1f433e578d7dfa1187347
cleeper24 <52640762+cleeper24@users.noreply.github.com>
2020-07-15 18:55:58 -0600

    course

--- openacademy/models/Course.py
+++ openacademy/models/Course.py
@@ -5,4 +5,7 @@ class Course(models.Model):
     _name = 'openacademy.course'
     _description = "OpenAcademy Courses"
 
+     name = fields.Char(string="Title", required=True)
+    description = fields.Text() 
+
    
