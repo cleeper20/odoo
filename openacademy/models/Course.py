962e1027a3afd49af0f45ac03615546171f702d1
cleeper24 <52640762+cleeper24@users.noreply.github.com>
2020-07-15 18:18:54 -0600

    course

--- openacademy/models/Course.py
+++ openacademy/models/Course.py
@@ -4,7 +4,6 @@ from odoo import models, fields, api
 class Course(models.Model):
     _name = 'openacademy.course'
     _description = "OpenAcademy Courses"
-
     name = fields.Char(string="Title", required=True)
     description = fields.Text()    
          responsible_id = fields.Many2one('res.users',
@@ -15,12 +14,10 @@ class Course(models.Model):
 class Session(models.Model):
     _name = 'openacademy.session'
     _description = "OpenAcademy Sessions"
-
     name = fields.Char(required=True)
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    
     instructor_id = fields.Many2one('res.partner', string="Instructor")
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
