155b7356b3b1758ced049b95eda54d6bd2657f7e
cleeper24 <52640762+cleeper24@users.noreply.github.com>
2020-07-25 01:09:13 -0600

    respaldo sin wizard

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -21,17 +21,17 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-        'depends': ['base', 'board'],
+    'depends': ['base', 'board'],#modulo que necesito
 
     # always loaded
     'data': [
-        'security/security.xml',
+       'security/security.xml',
         'security/ir.model.access.csv',
         'views/openacademy.xml',
         'views/partner.xml',
         'views/session_board.xml',
-        'reports.xml',
-    ],
+        'reports.xml',    
+],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
