cf3c3a2ca86343083469685f87842af80dad3d10
cleeper24 <52640762+cleeper24@users.noreply.github.com>
2020-07-24 19:01:00 -0600

    lfa curse

--- openacademy/models/Course.py
+++ openacademy/models/Course.py
@@ -21,17 +21,22 @@ class Session(models.Model):
     start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    active = fields.Boolean(default=True)   
+    active = fields.Boolean(default=True)
+    color = fields.Integer()
 
-      instructor_id = fields.Many2one('res.partner', string="Instructor",
+    instructor_id = fields.Many2one('res.partner', string="Instructor",
         domain=['|', ('instructor', '=', True),
-                     ('category_id.name', 'ilike', "Teacher")])        
+                     ('category_id.name', 'ilike', "Teacher")])
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
-    attendee_ids = fields.Many2many('res.partner', string="Attendees")    
-
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    end_date = fields.Date(string="End Date", store=True,
+        compute='_get_end_date', inverse='_set_end_date')
+
+    attendees_count = fields.Integer(
+        string="Attendees count", compute='_get_attendees_count', store=True)
 
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
